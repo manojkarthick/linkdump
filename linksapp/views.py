b84f2cfe66d0758d2546e8e0fbdc50f716dc0f7c
manojkarthick <manojkarthick@ymail.com>
2017-05-27 11:50:58 +0530

    Formatting index page

--- linksapp/views.py
+++ linksapp/views.py
@@ -5,7 +5,7 @@ from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 from django.contrib.auth.decorators import login_required
@@ -14,20 +14,20 @@ from django.http import HttpResponse
 from bs4 import BeautifulSoup
 import requests
 import json
-from datetime import datetime
+# from datetime import datetime
+
 
 @login_required
 def quickadd(request):
     if request.method == 'POST':
         url = request.POST['the_url']
         r = requests.get(url)
-        html = BeautifulSoup(r.text,'html.parser')
+        html = BeautifulSoup(r.text, 'html.parser')
         if html.title and html.title.text:
             title = html.title.text
         else:
             title = url
-        
-        link = Link(title=title,url=url,owner=request.user)
+        link = Link(title=title, url=url, owner=request.user)
         link.save()
 
         response_data = {}
@@ -44,58 +44,66 @@ def quickadd(request):
     else:
         return HttpResponse(json.dumps({"status": "some error encountered"}),content_type='application/json')
 
+
 @login_required
 def index(request):
     links = Link.objects.filter(owner=request.user).order_by('-date_modified')[:3]
     context = {'links': links }
-    return render(request,'linksapp/index.html',context)
+    return render(request, 'linksapp/index.html', context)
+
 
 @login_required
 def update_index_table(request):
     links = Link.objects.filter(owner=request.user).order_by('-date_modified')[:3]
-    context = {'links': links }
-    return render(request,'linksapp/list.html',context)
+    context = {'links': links}
+    return render(request, 'linksapp/list.html', context)
+
 
 @login_required
 def set_tag_hrchy(request):
     context = {}
-    return render(request,'linksapp/tag_hierarchy.html',context)
+    return render(request, 'linksapp/tag_hierarchy.html', context)
 
 
-class AllView(LoginRequiredMixin,generic.ListView):
+class AllView(LoginRequiredMixin, generic.ListView):
     template_name = 'linksapp/all.html'
     context_object_name = 'links'
 
     def get_queryset(self):
         return Link.objects.filter(owner=self.request.user).order_by('-date_modified')
 
-class DetailView(LoginRequiredMixin,generic.DetailView):
+
+class DetailView(LoginRequiredMixin, generic.DetailView):
     model = Link
     template_name = 'linksapp/detail.html'
 
 
-class LinkCreate(LoginRequiredMixin,CreateView):
+class LinkCreate(LoginRequiredMixin, CreateView):
     model = Link
     fields = ['title', 'url', 'description', 'tags', 'shortened_url']
     template_name = 'linksapp/create.html'
 
-class LinkDelete(LoginRequiredMixin,DeleteView):
+
+class LinkDelete(LoginRequiredMixin, DeleteView):
     model = Link
     template_name = 'linksapp/delete.html'
     success_url = reverse_lazy('linksapp:index')
 
-class LinkUpdate(LoginRequiredMixin,UpdateView):
+
+class LinkUpdate(LoginRequiredMixin, UpdateView):
     model = Link
     template_name = 'linksapp/update.html'
     fields = ['title', 'url', 'description', 'tags', 'shortened_url']
     success_url = reverse_lazy('linksapp:index')
 
+
 @login_required
-def increment_hits(request,link_id):
+def increment_hits(request, link_id):
     link = Link.objects.get(pk=link_id)
     link.hits += 1
     link.save()
     return redirect(link.url)
+
 
 def signup(request):
     if request.method == 'POST':
@@ -105,9 +113,9 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request,user)
+            login(request, user)
             return redirect('index')
     else:
         form = SignUpForm()
-    
-    return render(request,'registration/signup.html',{'form': form})
+
+    return render(request, 'registration/signup.html', {'form': form})
